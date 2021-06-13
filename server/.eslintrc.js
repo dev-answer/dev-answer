24a16e3034ad523b2c85c21e0accdc7ae837320e
parkinseo <inseo@finda.co.kr>
2021-06-13 17:37:00 +0900

    chore: 필요한 패키지 설치
    - typescript
    - eslint
    - express
    - nodemon 등

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: { },
+};
---
+++ server/start.sh
@@ -0,0 +1,2 @@
+tsc
+node dist/app.js
