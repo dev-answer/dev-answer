30c79c30f8544f73f74aed8f827715e118016504
passwd10 <parkinseo94@naver.com>
2021-06-19 14:41:08 +0900

    chore: server test url 수정

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,5 +23,5 @@ app.use('/graphql', graphqlHTTP({
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`Example app listening at http://localhost:${PORT}/graphql`);
 });
