faf6f949b5461e5e24d459d38f9fd52451fa5398
passwd10 <parkinseo94@naver.com>
2021-07-14 00:11:36 +0900

    feat: Comment resolver 구현
    - 문제 ID로 댓글목록 가져오는 resolver

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -4,17 +4,20 @@ import authResolver from './auth/authResolver';
 import userResolver from './user/userResolver';
 import questionCategoryResolver from './questionCategory/questionCategoryResolver';
 import questionResolver from './question/questionResolver';
+import commentResolver from './comment/commentResolver';
 
 import questionCategoryType from './questionCategory/questionCategoryType';
 import questionType from './question/questionType';
 import userType from './user/userType';
 import authType from './auth/authType';
+import commentType from './comment/commentType';
 
 export const typeDefs = mergeTypeDefs([
   questionCategoryType,
   questionType,
   userType,
   authType,
+  commentType,
 ]);
 
 export const resolvers = mergeResolvers([
@@ -22,4 +25,5 @@ export const resolvers = mergeResolvers([
   userResolver,
   questionCategoryResolver,
   questionResolver,
+  commentResolver,
 ]);
