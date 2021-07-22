2f47028f7efef57c560a5ca07bb902934e3f9b2c
Woomin-Jeon <dal96k@gmail.com>
2021-07-22 22:14:41 +0900

    fix: QueryRenderer -> useLazyLoadQuery 로 변경
    로그인 하는 작업은 렌더링이 따로 필요없는 작업이기에 더 명시적인 훅을 사용하는걸로 교체함

--- client/src/page/LoginCallbackPage.tsx
+++ client/src/page/LoginCallbackPage.tsx
@@ -1,48 +1,28 @@
 import React, { useEffect } from 'react';
-import { QueryRenderer, graphql } from 'react-relay';
+import { graphql, useLazyLoadQuery } from 'react-relay';
 import { useLocation } from 'react-router-dom';
 
-import Environment from '../graphql';
+import withSuspense from '../hocs/withSuspense';
 import { LoginCallbackPageQuery } from '../__generated__/LoginCallbackPageQuery.graphql';
 
 import APP_DOMAIN from '../constants/domain';
 
 const LoginCallbackPage: React.FC = () => {
   const { search } = useLocation();
-
   const searchParams = new URLSearchParams(search);
   const code = searchParams.get('code') || '';
 
-  return (
-    <QueryRenderer<LoginCallbackPageQuery>
-      environment={Environment}
-      query={graphql`
-        mutation LoginCallbackPageQuery($code: String!) {
-          login(code: $code) {
-            accessToken
-          }
-        }
-      `}
-      variables={{ code }}
-      render={({ error, props }) => {
-        if (error) {
-          return '로그인에 실패했습니다.';
-        }
+  const query = useLazyLoadQuery<LoginCallbackPageQuery>(graphql`
+    mutation LoginCallbackPageQuery($code: String!) {
+      login(code: $code) {
+        accessToken
+      }
+    }
+  `, { code });
 
-        if (!props) {
-          return '로그인을 하는 중입니다...';
-        }
-
-        const { accessToken } = props.login;
-
-        return <LoginResult accessToken={accessToken} />;
-      }}
-    />
-  );
-};
-
-const LoginResult: React.FC<{ accessToken: string | null }> = ({ accessToken }) => {
   useEffect(() => {
+    const { accessToken } = query.login;
+
     window.opener?.postMessage({ accessToken }, APP_DOMAIN);
     window.close();
   }, []);
@@ -50,4 +30,4 @@ const LoginResult: React.FC<{ accessToken: string | null }> = ({ accessToken }) 
   return null;
 };
 
-export default LoginCallbackPage;
+export default withSuspense(LoginCallbackPage, () => <div>로딩중...</div>);
