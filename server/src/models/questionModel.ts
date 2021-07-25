d5178ea2eb36c83e4ef6a671a43a2ad997b2df77
daadaadaah <daadaadaah@gmail.com>
2021-07-26 01:09:59 +0900

    feat: questionModel에findOneById 메서드 추가 및 타입 추가

--- server/src/models/questionModel.ts
+++ server/src/models/questionModel.ts
@@ -1,17 +1,31 @@
 import fs from 'fs';
 import path from 'path';
 
+interface Question {
+  id: number;
+  title: string;
+  content: string;
+  category: string;
+  level: number;
+  frequency: boolean;
+}
+
 export default class QuestionModel {
   questionsFile;
 
-  questions;
+  questions: [Question];
 
   constructor() {
     this.questionsFile = fs.readFileSync(path.join(__dirname, '../db/question.json'), 'utf-8');
     this.questions = JSON.parse(this.questionsFile);
   }
 
-  findMany() {
+  findOneById(questionId: number): Question {
+    const questions = this.questions.filter((question: Question) => question.id === questionId);
+    return questions[0];
+  }
+
+  findMany(): [Question] {
     return this.questions;
   }
 }
