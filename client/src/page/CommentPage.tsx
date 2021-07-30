64af00ff77c79079dda547ab5c34c1e2e24c8f92
passwd10 <parkinseo94@naver.com>
2021-07-30 21:01:21 +0900

    feat: 댓글을 입력할 수 있는 Form을 추가한다

--- client/src/page/CommentPage.tsx
+++ client/src/page/CommentPage.tsx
@@ -1,49 +1,26 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { QueryRenderer, graphql } from 'react-relay';
 
 import Environment from '../graphql';
-import { CommentPageQuery, CommentPageQueryResponse } from '../__generated__/CommentPageQuery.graphql';
+import { CommentPageQuery } from '../__generated__/CommentPageQuery.graphql';
 
 import Comment from '../components/Example/Comment';
 
-interface IRenderQuery {
-  error: Error | null;
-  props: CommentPageQueryResponse | null;
-}
+const CommentPage: React.FC = () => {
+  const [commentInput, setCommentInput] = useState('');
 
-const renderQuery = ({ error, props }: IRenderQuery) => {
-  if (!props) {
-    return <div>...로딩중</div>;
-  }
+  const handleOnChangeInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setCommentInput(event.target.value);
+  }, []);
 
-  if (error) {
-    return <div>...에러발생</div>;
-  }
+  const handleOnSubmit = useCallback((event: React.FormEvent) => {
+    event.preventDefault();
+  }, []);
 
   return (
-    <section>
-      <h2>
-        댓글
-      </h2>
-      {props.comments.length === 0
-        ? <p>댓글이 없습니다</p>
-        : (
-          <ol>
-            {props.comments.map((comment) => (
-              <li key={comment.id}>
-                <Comment comment={comment} />
-              </li>
-            ))}
-          </ol>
-        )}
-    </section>
-  );
-};
-
-const CommentPage: React.FC = () => (
-  <QueryRenderer<CommentPageQuery>
-    environment={Environment}
-    query={graphql`
+    <QueryRenderer<CommentPageQuery>
+      environment={Environment}
+      query={graphql`
       query CommentPageQuery($questionId: Int) {
         comments(questionId: $questionId) {
           id
@@ -51,11 +28,52 @@ const CommentPage: React.FC = () => (
         }
       }
     `}
-    variables={{
-      questionId: 1,
-    }}
-    render={renderQuery}
-  />
-);
+      variables={{
+        questionId: 1,
+      }}
+      render={({ error, props }) => {
+        if (!props) {
+          return <div>...로딩중</div>;
+        }
+
+        if (error) {
+          return <div>...에러발생</div>;
+        }
+
+        return (
+          <section>
+            <h2>
+              댓글
+            </h2>
+            {props.comments.length === 0
+              ? <p>댓글이 없습니다</p>
+              : (
+                <ol>
+                  {props.comments.map((comment) => (
+                    <li key={comment.id}>
+                      <Comment comment={comment} />
+                    </li>
+                  ))}
+                </ol>
+              )}
+            <form onSubmit={handleOnSubmit}>
+              <label htmlFor="commentInput">
+                유저프로필
+                <input
+                  type="text"
+                  id="commentInput"
+                  placeholder="제 생각에는..."
+                  value={commentInput}
+                  onChange={handleOnChangeInput}
+                />
+              </label>
+              <input type="submit" value="등록" />
+            </form>
+          </section>
+        );
+      }}
+    />
+  );
+};
 
 export default CommentPage;
