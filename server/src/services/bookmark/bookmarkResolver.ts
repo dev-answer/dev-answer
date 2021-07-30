5f69b52d5ce759d8614899a588efb3a48dffe5aa
daadaadaah <daadaadaah@gmail.com>
2021-07-31 05:33:39 +0900

    fix: 불필요한 log 삭제

--- server/src/services/bookmark/bookmarkResolver.ts
+++ server/src/services/bookmark/bookmarkResolver.ts
@@ -16,14 +16,9 @@ export default {
     bookmarks: async (_:any, args: { userId: number }) => {
       const bookmarks = await bookmarkRepo.findAllById(args.userId);
 
-      console.log('BookmarkResolver bookmarks : ', bookmarks);
-
       // TODO : 나중에 PostgreSQL 사용시, IN절을 이용한 로직으로 로직 수정할 예정 해당 로직 해결할 예정
       const bookmarkWithQuestion = bookmarks.map(async (bookmark: Bookmark) => {
         const question = await questionRepo.findOneById(bookmark.questionId);
-
-        const temp = question[0];
-        console.log('BookmarkResolver question : ', temp);
 
         return {
           ...bookmark,
@@ -32,8 +27,6 @@ export default {
       });
 
       const results = await Promise.all(bookmarkWithQuestion);
-
-      console.log('BookmarkResolver results : ', results);
 
       return results;
     },
