f9e55d0992125684d0cf67ba12cc90a8e55d1099
passwd10 <parkinseo94@naver.com>
2021-07-31 14:12:41 +0900

    feat: QueryRenderer를 hook 으로 변경한다

--- client/src/page/CommentPage.tsx
+++ client/src/page/CommentPage.tsx
@@ -1,12 +1,30 @@
-import React, { useState, useCallback } from 'react';
-import { QueryRenderer, graphql } from 'react-relay';
+import React, {
+  useState, useCallback, Suspense, useEffect,
+} from 'react';
+import {
+  graphql, usePreloadedQuery, useQueryLoader, PreloadedQuery,
+} from 'react-relay';
 
-import Environment from '../graphql';
 import { CommentPageQuery } from '../__generated__/CommentPageQuery.graphql';
 
 import Comment from '../components/Example/Comment';
 
-const CommentPage: React.FC = () => {
+type Props = {
+  commentQueryRef: PreloadedQuery<CommentPageQuery>
+}
+
+const CommentQuery = graphql`
+  query CommentPageQuery($questionId: Int) {
+    comments(questionId: $questionId) {
+      id
+      ...Comment_comment
+    }
+  }
+`;
+
+const CommentContainer: React.FC<Props> = ({ commentQueryRef }) => {
+  const { comments } = usePreloadedQuery(CommentQuery, commentQueryRef);
+
   const [commentInput, setCommentInput] = useState('');
 
   const handleOnChangeInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,61 +36,63 @@ const CommentPage: React.FC = () => {
   }, []);
 
   return (
-    <QueryRenderer<CommentPageQuery>
-      environment={Environment}
-      query={graphql`
-      query CommentPageQuery($questionId: Int) {
-        comments(questionId: $questionId) {
-          id
-          ...Comment_comment
-        }
-      }
-    `}
-      variables={{
-        questionId: 1,
-      }}
-      render={({ error, props }) => {
-        if (!props) {
-          return <div>...로딩중</div>;
-        }
+    <section>
+      <h2>
+        댓글
+      </h2>
+      {comments.length === 0
+        ? <p>댓글이 없습니다</p>
+        : (
+          <ol>
+            {comments.map((comment) => (
+              <li key={comment.id}>
+                <Comment comment={comment} />
+              </li>
+            ))}
+          </ol>
+        )}
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="commentInput">
+          유저프로필
+          <input
+            type="text"
+            id="commentInput"
+            placeholder="제 생각에는..."
+            value={commentInput}
+            onChange={handleOnChangeInput}
+          />
+        </label>
+        <input type="submit" value="등록" />
+      </form>
+    </section>
+  );
+};
 
-        if (error) {
-          return <div>...에러발생</div>;
-        }
+const CommentPage: React.FC = () => {
+  const [
+    commentQueryRef,
+    loadQuery,
+    disposeQuery,
+  ] = useQueryLoader<CommentPageQuery>(CommentQuery);
 
-        return (
-          <section>
-            <h2>
-              댓글
-            </h2>
-            {props.comments.length === 0
-              ? <p>댓글이 없습니다</p>
-              : (
-                <ol>
-                  {props.comments.map((comment) => (
-                    <li key={comment.id}>
-                      <Comment comment={comment} />
-                    </li>
-                  ))}
-                </ol>
-              )}
-            <form onSubmit={handleOnSubmit}>
-              <label htmlFor="commentInput">
-                유저프로필
-                <input
-                  type="text"
-                  id="commentInput"
-                  placeholder="제 생각에는..."
-                  value={commentInput}
-                  onChange={handleOnChangeInput}
-                />
-              </label>
-              <input type="submit" value="등록" />
-            </form>
-          </section>
-        );
-      }}
-    />
+  useEffect(() => {
+    loadQuery({ questionId: 1 });
+    return () => {
+      disposeQuery();
+    };
+  }, [loadQuery, disposeQuery]);
+
+  return (
+    <>
+      <Suspense fallback="로딩중..">
+        {commentQueryRef != null
+          ? (
+            <CommentContainer
+              commentQueryRef={commentQueryRef}
+            />
+          ) : null}
+      </Suspense>
+    </>
   );
 };
 
