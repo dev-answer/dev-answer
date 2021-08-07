d97501d495bc6842aaac3379602d46c9c5a3002a
daadaadaah <daadaadaah@gmail.com>
2021-08-08 04:25:56 +0900

    chore: Sentry 도입
    See also:
    - https://docs.sentry.io/platforms/javascript/

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -20,6 +20,18 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+if (process.env.NODE_ENV === 'production') {
+  const tempDSN = 'https://06861ab796e04e7494fd47f3b8b5e5e3@o948125.ingest.sentry.io/5897330';
+
+  Sentry.init({
+    // dns: process.env.SENTRY_DSN,
+    dsn: tempDSN, // TODO : CI/CD 구축 후 process.env.SENTRY_DSN 로 관리하기
+    integrations: [new Integrations.BrowserTracing()],
+    environment: process.env.NODE_ENV,
+    tracesSampleRate: 1.0,
+  });
+}
+
 const rootElement = document.querySelector('#root');
 
 ReactDOM.render(
