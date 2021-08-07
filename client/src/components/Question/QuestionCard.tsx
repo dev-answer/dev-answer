b3bbb1e5d15f93c1006df7f3f6f6f984bba50749
Woomin-Jeon <dal96k@gmail.com>
2021-08-07 18:10:28 +0900

    fix: 디자인이 바뀜에 따라 불필요해진 코드들 제거

--- client/src/components/Question/QuestionCard.tsx
+++ client/src/components/Question/QuestionCard.tsx
@@ -1,5 +1,5 @@
+import React, { MouseEvent } from 'react';
 import styled from '@emotion/styled';
-import React, { useEffect, useState } from 'react';
 import { graphql, useFragment } from 'react-relay';
 
 import { QuestionCard_question$key } from '__generated__/QuestionCard_question.graphql';
@@ -11,35 +11,30 @@ const questionCardFragment = graphql`
   }
 `;
 
-type Size = 'small' | 'middle'
-
 interface Props {
   questionRef: QuestionCard_question$key
-  size: Size
   onClick: () => void
 }
 
-const QuestionCard: React.FC<Props> = ({ questionRef, size, onClick }) => {
+const QuestionCard: React.FC<Props> = ({ questionRef, onClick }) => {
   const {
     content, category,
   } = useFragment<QuestionCard_question$key>(questionCardFragment, questionRef);
-  const [animation, setAnimation] = useState(false);
 
-  useEffect(() => {
-    if (size === 'middle') {
-      setAnimation(true);
-    }
-  }, []);
+  const handleClickCard = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    onClick();
+  };
 
   return (
-    <Base onClick={onClick} animation={animation} size={size}>
+    <Card onClick={handleClickCard}>
       <Content>{content}</Content>
       <Category>{category}</Category>
-    </Base>
+    </Card>
   );
 };
 
-const Base = styled.section<{ animation: boolean; size: Size }>`
+const Card = styled.a`
   display: flex;
   flex-direction: column;
   transition: 300ms;
@@ -49,37 +44,14 @@ const Base = styled.section<{ animation: boolean; size: Size }>`
   box-sizing: border-box;
   margin: 15px 10px;
   cursor: pointer;
-  ${({ animation, size }) => {
-    if (size === 'small') {
-      return `
-        width: 276px;
-        height: 322px;
-      `;
-    }
+  width: 277px;
+  height: 325px;
 
-    if (size === 'middle') {
-      if (animation) {
-        return `
-          transform: scale(1);
-          width: 573px;
-          height: 664px;
-        `;
-      }
-
-      return `
-        transform: scale(0.5);
-        width: 276px;
-        height: 322px;
-      `;
-    }
-
-    return null;
-  }};
   &:hover {
     background: #D7D7D7
   }
 `;
-const Content = styled.h2`
+const Content = styled.p`
   color: #757575;
 `;
 const Category = styled.p`
--- client/src/page/QuestionBoardPage.tsx
+++ client/src/page/QuestionBoardPage.tsx
@@ -1,14 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import { graphql, useLazyLoadQuery } from 'react-relay';
 
 import { QuestionBoardPageQuery } from '__generated__/QuestionBoardPageQuery.graphql';
 
 import styled from '@emotion/styled';
-import { groupArray, makeGroupElementOneself } from '../utils/array';
 import withSuspense from '../hocs/withSuspense';
 import QuestionCard from '../components/Question/QuestionCard';
-
-const COULMN_LINE_COUNT = 2;
 
 const questionBoardPageQuery = graphql`
   query QuestionBoardPageQuery {
@@ -21,52 +18,20 @@ const questionBoardPageQuery = graphql`
 
 const QuestionBoardPage: React.FC = () => {
   const questionsRef = useLazyLoadQuery<QuestionBoardPageQuery>(questionBoardPageQuery, {});
-  const [selectedQuestionId, setSelectedQuestionId] = useState<null | string>(null);
   const baseRef = useRef<HTMLDivElement>(null);
 
-  const questionsGroupedByColumnLineCount = groupArray(
-    questionsRef.allQuestions,
-    COULMN_LINE_COUNT,
-  );
-
-  const { groupedArray, oneselfIndex } = makeGroupElementOneself(
-    questionsGroupedByColumnLineCount,
-    (e) => e.id === selectedQuestionId,
-  );
-
-  const handleClickSmallQuestion = (id: string) => () => {
-    setSelectedQuestionId(id);
+  const handleClickQuestionCard = () => {
+    // TODO: 문제 상세 페이지로 이동
   };
 
   return (
     <Base ref={baseRef}>
-      {groupedArray.map((groupedElement, index) => (
-        <div key={`group-${groupedElement[0].id}`}>
-          {(() => {
-            if (index === oneselfIndex) {
-              return (
-                <QuestionCard
-                  questionRef={groupedElement[0]}
-                  size="middle"
-                  onClick={handleClickSmallQuestion(groupedElement[0].id)}
-                />
-              );
-            }
-
-            return (
-              <>
-                {groupedElement.map((questionRef) => (
-                  <QuestionCard
-                    key={questionRef.id}
-                    size="small"
-                    questionRef={questionRef}
-                    onClick={handleClickSmallQuestion(questionRef.id)}
-                  />
-                ))}
-              </>
-            );
-          })()}
-        </div>
+      {questionsRef.allQuestions.map((questionRef) => (
+        <QuestionCard
+          key={questionRef.id}
+          questionRef={questionRef}
+          onClick={handleClickQuestionCard}
+        />
       ))}
     </Base>
   );
