6ccb038ebdd9b3ccd36d7deaa53c779bba7d6d37
daadaadaah <daadaadaah@gmail.com>
2021-08-14 09:24:53 +0900

    feat: 기본 <Fallback /> 추가

--- client/src/helper/Errorboundary.tsx
+++ client/src/helper/Errorboundary.tsx
@@ -1,4 +1,5 @@
-import { Component, ErrorInfo, ReactNode } from 'react';
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+
 import * as Sentry from '@sentry/react';
 
 interface Props {
@@ -9,6 +10,20 @@ interface Props {
 interface State {
   error?: Error;
 }
+
+// TODO : 현재 임시로 구현해 놓은 상태. 디자인 나오면 UI 수정 예정
+const Fallback = () => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    alert('홈화면으로 가자!');
+  };
+
+  return (
+    <>
+      <h1>예상치 못한 에러가 발생하였습니다!</h1>
+      <button type="button" onClick={handleClick}>홈 화면으로</button>
+    </>
+  );
+};
 
 export default class ErrorBoundary extends Component<Props, State> {
   constructor(props: Props) {
@@ -41,10 +56,10 @@ export default class ErrorBoundary extends Component<Props, State> {
 
   render() {
     const { error } = this.state;
-    const { fallback, children } = this.props;
+    const { children } = this.props;
 
     if (error) {
-      return fallback;
+      return <Fallback />;
     }
 
     return children;
