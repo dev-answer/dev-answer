d2285ce039a4d20a2442f9b39e1cf72ced1600a8
Woomin-Jeon <dal96k@gmail.com>
2021-08-15 15:36:05 +0900

    feat: access token을 관리하는 로컬스토리지 키를 contants로 분리

--- client/src/constants/domain.ts
+++ client/src/constants/domain.ts
@@ -1,4 +1,2 @@
 export const APP_DOMAIN = process.env.APP_DOMAIN || 'http://localhost:8080';
-
-// TODO: Remove
-export default APP_DOMAIN;
+export const LOCALSTORAGE_ACCESS_TOKEN_KEY = 'auth/access-token';
