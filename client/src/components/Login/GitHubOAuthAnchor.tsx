18caa1f47185f7ae9d2d3582fd6f89e0bf70641e
Woomin-Jeon <dal96k@gmail.com>
2021-08-15 15:39:33 +0900

    feat: GitHubOAuthAnchor를 범용성 있게 수정 후 헤더에 적용

--- client/src/components/Login/GitHubOAuthAnchor.tsx
+++ client/src/components/Login/GitHubOAuthAnchor.tsx
@@ -1,8 +1,13 @@
 import React, { MouseEvent } from 'react';
+import styled from '@emotion/styled';
 
 import { APP_DOMAIN } from '../../constants/domain';
 
-const GitHubOAuthAnchor: React.FC = () => {
+interface Props {
+  className?: string
+}
+
+const GitHubOAuthAnchor: React.FC<Props> = ({ children, className }) => {
   const CLIENT_ID = process.env.GITHUB_OAUTH_CLIENT_ID;
   const OAUTH_REDIRECT_URL = `${APP_DOMAIN}/oauth`;
   const OAUTH_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${OAUTH_REDIRECT_URL}`;
@@ -14,8 +19,18 @@ const GitHubOAuthAnchor: React.FC = () => {
   };
 
   return (
-    <a href={OAUTH_LOGIN_URL} onClick={handleClickLogin}>Login</a>
+    <LoginAnchor
+      className={className}
+      href={OAUTH_LOGIN_URL}
+      onClick={handleClickLogin}
+    >
+      {children}
+    </LoginAnchor>
   );
 };
+
+const LoginAnchor = styled.a`
+  text-decoration: none;
+`;
 
 export default GitHubOAuthAnchor;
