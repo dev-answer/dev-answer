beabb9e95baad356742ea5c45612003dcee0de18
Woomin-Jeon <dal96k@gmail.com>
2021-08-15 17:26:32 +0900

    feat: 유저를 DB에 추가하는 로직 구현

--- server/src/repositories/userRepository.ts
+++ server/src/repositories/userRepository.ts
@@ -2,8 +2,15 @@ import UserModel from '../models/userModel';
 
 import Repository from './repository';
 
+import { User } from '../types/user';
+
 export default class UserRepository extends Repository {
   constructor() {
     super({ Model: new UserModel() });
+  }
+
+  async create(user: Omit<User, 'id'>) {
+    const result = await this.collection.create(user);
+    return result;
   }
 }
