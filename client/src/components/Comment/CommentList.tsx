e2135dfcd01baa33f3b7ff3e3b4df4ca74a314fb
passwd10 <parkinseo94@naver.com>
2021-08-19 20:47:38 +0900

    refactor: Guard Clause를 적용한다

--- client/src/components/Comment/CommentList.tsx
+++ client/src/components/Comment/CommentList.tsx
@@ -29,20 +29,20 @@ const CommentListContainer: React.FC<Props> = ({ commentQueryRef }) => {
 
   const isLoggedIn = localStorage.getItem(LOCALSTORAGE_ACCESS_TOKEN_KEY);
 
+  if (!comments.length) {
+    return <p>댓글이 없습니다</p>;
+  }
+
   if (!isLoggedIn) {
     return (
       <div>
-        {comments.length === 0
-          ? <p />
-          : (
-            <ol>
-              {comments.slice(0, 1).map((comment) => (
-                <li key={comment.id}>
-                  <Comment comment={comment} />
-                </li>
-              ))}
-            </ol>
-          )}
+        <ol>
+          {comments.slice(0, 1).map((comment) => (
+            <li key={comment.id}>
+              <Comment comment={comment} />
+            </li>
+          ))}
+        </ol>
         <p>
           다른 사람의 답변이 궁금하다면?
         </p>
@@ -52,19 +52,13 @@ const CommentListContainer: React.FC<Props> = ({ commentQueryRef }) => {
   }
 
   return (
-    <div>
-      {comments.length === 0
-        ? <p>댓글이 없습니다</p>
-        : (
-          <ol>
-            {comments.map((comment) => (
-              <li key={comment.id}>
-                <Comment comment={comment} />
-              </li>
-            ))}
-          </ol>
-        )}
-    </div>
+    <ol>
+      {comments.map((comment) => (
+        <li key={comment.id}>
+          <Comment comment={comment} />
+        </li>
+      ))}
+    </ol>
   );
 };
 
