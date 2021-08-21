4164c1c8e8f21d09c0552166c26aaec07655984c
Woomin-Jeon <dal96k@gmail.com>
2021-08-21 16:37:16 +0900

    fix: 불필요한 프라이빗 함수들 제거

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 import { User } from '../types';
+import { writeJSON } from '../utils';
 
 export default class UserModel {
   usersFile;
@@ -13,40 +14,18 @@ export default class UserModel {
     this.users = JSON.parse(this.usersFile);
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  private writeFile(data: any) {
-    try {
-      fs.writeFileSync(path.join(__dirname, '../db/user.json'), JSON.stringify(data), 'utf-8');
-    } catch (error) {
-      throw Error(`${error}파일 작성에 실패했습니다`);
-    }
-  }
-
-  private find(callback: (user: User) => boolean) {
-    const targetUser = this.users.find(callback);
-
-    if (!targetUser) {
-      return null;
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { accessToken, gitHubAccessToken, ...userInfo } = targetUser;
-
-    return userInfo;
-  }
-
   findMany() {
     return this.users;
   }
 
   findOneByUserId(userId: string) {
-    const targetUser = this.find((user) => user.id === userId);
+    const targetUser = this.users.find((user) => user.id === userId);
 
     return targetUser;
   }
 
   findOneByAccessToken(accessToken: string) {
-    const targetUser = this.find((user) => user.accessToken === accessToken);
+    const targetUser = this.users.find((user) => user.accessToken === accessToken);
 
     return targetUser;
   }
@@ -54,7 +33,7 @@ export default class UserModel {
   createOne(newUser: User) {
     this.users = [...this.users, newUser];
 
-    this.writeFile(this.users);
+    writeJSON(path.join('../db/user.json'), this.users);
 
     return newUser;
   }
@@ -63,7 +42,7 @@ export default class UserModel {
     const updatedUser: User = { ...user, ...paylaod };
     this.users = this.users.map((dbUser) => (dbUser.id === user.id ? updatedUser : dbUser));
 
-    this.writeFile(this.users);
+    writeJSON(path.join('../db/user.json'), this.users);
 
     return updatedUser;
   }
