097e51ff4b6a6f719eaacf4a82e23c6989acdd05
passwd10 <parkinseo94@naver.com>
2021-08-22 00:36:22 +0900

    feat: Comment에 간단한 마크업을 적용한다
    - + 좋아요, 싫어요를 숫자로 표기

--- client/src/components/Comment/Comment.tsx
+++ client/src/components/Comment/Comment.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 
+import styled from '@emotion/styled';
+
 import { Comment_comment } from '__generated__/Comment_comment.graphql';
 
 import UserInfo from '../common/UserInfo';
@@ -9,29 +11,32 @@ interface Props {
   comment: Comment_comment
 }
 
-const Comment: React.FC<Props> = ({ comment }) => (
-  <li>
-    <div>
-      유저 :
-      {' '}
-      {comment.uid && <UserInfo uid={comment.uid} />}
-    </div>
-    <div>
-      {comment.content}
-    </div>
-    <div>
-      좋아요 :
-      {' '}
-      {comment.like}
-    </div>
-    <div>
-      싫어요 :
-      {' '}
-      {comment.dislike}
-    </div>
-  </li>
-);
+const Comment: React.FC<Props> = ({ comment }) => {
+  const {
+    uid, like, dislike, content,
+  } = comment;
 
+  if (!uid) {
+    return <p>유저 정보를 불러오지 못했습니다</p>;
+  }
+
+  return (
+    <CommentArea>
+      <UserInfo uid={uid} />
+      <div>
+        좋아요 :
+        {like?.length}
+      </div>
+      <div>
+        싫어요 :
+        {dislike?.length}
+      </div>
+      <p>
+        {content}
+      </p>
+    </CommentArea>
+  );
+};
 export default createFragmentContainer(Comment, {
   comment: graphql`
     fragment Comment_comment on Comment{
@@ -45,3 +50,9 @@ export default createFragmentContainer(Comment, {
     }
   `,
 });
+
+const CommentArea = styled.li`
+  border: 1px solid gray;
+  margin: 10px;
+  padding: 10px;
+`;
