60b1c689d0192b250ca31cb170e80600585e2718
Woomin-Jeon <dal96k@gmail.com>
2021-10-16 17:02:03 +0900

    fix: 잘못된 디폴트 도메인 수정

--- client/src/constants/domain.ts
+++ client/src/constants/domain.ts
@@ -1,2 +1,3 @@
-export const APP_DOMAIN = process.env.APP_DOMAIN || 'http://localhost:3080';
+export const APP_DOMAIN = process.env.APP_DOMAIN || 'http://localhost:8080';
+
 export const LOCALSTORAGE_ACCESS_TOKEN_KEY = 'auth/access-token';
